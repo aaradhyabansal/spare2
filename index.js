b274d6be4a02278b0671d33d78c8e1f0480469ed
aaradhyabansal <aaradhyabansal21@gmail.com>
2024-09-23 23:52:41 +0530

    added skeleton routes

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-dedfrfrf;
+const express = require("express");
+const app = express();
+
+app.post("/user/signup", (req, res) => {
+  res.json({
+    message: "user signup endpoint",
+  });
+});
+
+app.post("user/signin", function (req, res) {
+  res.json({
+    message: "user signin endpoint",
+  });
+});
+
+app.get("/courses", (req, res) => {
+  res.json({
+    message: "all courses endpoint",
+  });
+});
+
+app.post("/user/purchase", (req, res) => {
+  res.json({
+    message: "courses purchase endpoint",
+  });
+});
+
+app.get("/user/purchases", (res, res) => {
+  res.json({
+    message: " users courses endpoint",
+  });
+});
+
+app.listen(3000);
